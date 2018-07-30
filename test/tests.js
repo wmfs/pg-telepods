6980e04f1cd265e3d91a0fb777a65b03b73f63ec
Jez Higgins <jez@jezuk.co.uk>
2018-07-30 13:39:22 +0100

    test: Added extra describe sections

--- test/tests.js
+++ test/tests.js
@@ -22,87 +22,92 @@ describe('Run the basic-usage example',
       }
     })
 
-    it('Should create a new pg client', () => {
-      client = new HlPgClient(process.env.PG_CONNECTION_STRING)
-    })
-
-    it('Should install test schemas', () => {
-      return client.runFile(path.resolve(__dirname, 'fixtures', 'install-test-schemas.sql'))
-    })
-
-    it('Should start the telepods', async () => {
-      const result = await startTelepods({
-        client: client,
-        outputDir: path.resolve(__dirname, './output'),
-        source: {
-          tableName: 'springfield.people',
-          hashSumColumnName: 'hash_sum'
-        },
-        target: {
-          tableName: 'government.census',
-          hashSumColumnName: 'origin_hash_sum'
-        },
-        join: {
-          'social_security_id': 'social_security_id' // key = source table column, value = target table column
-        },
-        transformFunction: function (sourceRow, callback) {
-          callback(null, {
-            'socialSecurityId': sourceRow.socialSecurityId,
-            'name': sourceRow.firstName + ' ' + sourceRow.lastName,
-            'town': 'Springfield'
-          })
-        }
+    describe('set up', () => {
+      it('create a new pg client', () => {
+        client = new HlPgClient(process.env.PG_CONNECTION_STRING)
       })
-      expect(result).to.not.equal(null)
+
+      it('install test schemas', () => {
+        return client.runFile(path.resolve(__dirname, 'fixtures', 'install-test-schemas.sql'))
+      })
     })
 
-    it('Should return correctly modified children rows', async () => {
-      const result = await client.query(
-        'select social_security_id, origin_hash_sum, name, town from government.census order by social_security_id'
-      )
-      expect(result.rows).to.eql(
-        [
-          {
-            'name': 'Homer Simpson',
-            'origin_hash_sum': 'AAAAAAAA',
-            'social_security_id': 1,
-            'town': 'Springfield'
+    describe('test', () => {
+      it('start the telepods', async () => {
+        const result = await startTelepods({
+          client: client,
+          outputDir: path.resolve(__dirname, './output'),
+          source: {
+            tableName: 'springfield.people',
+            hashSumColumnName: 'hash_sum'
           },
-          {
-            'name': 'Marge Simpson',
-            'origin_hash_sum': 'BBBBBBBB',
-            'social_security_id': 2,
-            'town': 'Springfield'
+          target: {
+            tableName: 'government.census',
+            hashSumColumnName: 'origin_hash_sum'
           },
-          {
-            'name': 'Montgomery Burns',
-            'origin_hash_sum': 'EEEEEEEE',
-            'social_security_id': 5,
-            'town': 'Springfield'
+          join: {
+            'social_security_id': 'social_security_id' // key = source table column, value = target table column
           },
-          {
-            'name': 'Ned Flanders',
-            'origin_hash_sum': '11111111',
-            'social_security_id': 6,
-            'town': 'Springfield'
+          transformFunction: function (sourceRow, callback) {
+            callback(null, {
+              'socialSecurityId': sourceRow.socialSecurityId,
+              'name': sourceRow.firstName + ' ' + sourceRow.lastName,
+              'town': 'Springfield'
+            })
           }
-        ]
-      )
-      expect(result).to.not.equal(null)
+        })
+        expect(result).to.not.equal(null)
+      })
+
+      it('verify modified children rows', async () => {
+        const result = await client.query(
+          'select social_security_id, origin_hash_sum, name, town from government.census order by social_security_id'
+        )
+        expect(result.rows).to.eql(
+          [
+            {
+              'name': 'Homer Simpson',
+              'origin_hash_sum': 'AAAAAAAA',
+              'social_security_id': 1,
+              'town': 'Springfield'
+            },
+            {
+              'name': 'Marge Simpson',
+              'origin_hash_sum': 'BBBBBBBB',
+              'social_security_id': 2,
+              'town': 'Springfield'
+            },
+            {
+              'name': 'Montgomery Burns',
+              'origin_hash_sum': 'EEEEEEEE',
+              'social_security_id': 5,
+              'town': 'Springfield'
+            },
+            {
+              'name': 'Ned Flanders',
+              'origin_hash_sum': '11111111',
+              'social_security_id': 6,
+              'town': 'Springfield'
+            }
+          ]
+        )
+        expect(result).to.not.equal(null)
+      })
+
+      it('check for deletes csv', () => {
+        const censusDeletes = path.resolve(__dirname, './output/deletes/census.csv')
+        expect(fs.existsSync(censusDeletes)).to.equal(true)
+      })
     })
 
-    it('Should check for deletes csv', () => {
-      const censusDeletes = path.resolve(__dirname, './output/deletes/census.csv')
-      expect(fs.existsSync(censusDeletes)).to.equal(true)
-    })
+    describe('clean up', () => {
+      it('uninstall test schemas', () => {
+        return client.runFile(path.resolve(__dirname, 'fixtures', 'uninstall-test-schemas.sql'))
+      })
 
-    it('Should uninstall test schemas', () => {
-      return client.runFile(path.resolve(__dirname, 'fixtures', 'uninstall-test-schemas.sql'))
-    })
-
-    it('Should close database connections', function (done) {
-      client.end()
-      done()
+      it('close database connections', () => {
+        client.end()
+      })
     })
   }
 )
