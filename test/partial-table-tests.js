48025e33934124038599375fb19785f38242c9b4
Jez Higgins <jez@jezuk.co.uk>
2020-08-10 16:51:16 +0100

    fix: tweak conflicts select to handle nulls in the where conditions
    You can't just invert (x=y) to NOT(x=y) if x might be null. It has to be (NOT(x=y) OR X is NULL).
    Turns out to be easier to generate NOT(x=y and x is not null). It reads a little bit oddly, but the
    logic is exactly equivalent.

--- test/partial-table-tests.js
+++ test/partial-table-tests.js
@@ -42,6 +42,7 @@ describe('partial-table test',
     const secondSyncOutputDir = path.resolve(__dirname, './output', 'second-partial')
     const thirdSyncOutputDir = path.resolve(__dirname, './output', 'partial-with-hash-conflict')
     const fourthSyncOutputDir = path.resolve(__dirname, './output', 'partial-with-null-hash-conflict')
+    const fifthSyncOutputDir = path.resolve(__dirname, './output', 'partial-with-null-town-conflict')
 
     describe('sync first table', () => {
       it('start the telepods', async () => {
@@ -340,6 +341,57 @@ describe('partial-table test',
 
       it('check conflict csv has one row', () => {
         checkFileLength(fourthSyncOutputDir, 'conflicts', 'census.csv', 3)
+      })
+    })
+
+    describe('sync conflict with null target hash_sum and null town', () => {
+      it('create conflicting insert row - matching primary key, null hash, but wrong city', async () => {
+        await client.query('update government.census set origin_hash_sum = null, name = \'Lila\', town = null where id_number = 21')
+      })
+
+      it('start the telepods', async () => {
+        const result = await startTelepods({
+          client: client,
+          outputDir: fifthSyncOutputDir,
+          source: {
+            tableName: 'worldof.tomorrow',
+            hashSumColumnName: 'hash_sum'
+          },
+          target: {
+            tableName: 'government.census',
+            hashSumColumnName: 'origin_hash_sum',
+            where: {
+              town: { equals: 'New New York' }
+            }
+          },
+          join: {
+            space_id: 'id_number' // key = source table column, value = target table column
+          },
+          transformFunction: function (sourceRow, callback) {
+            callback(null, {
+              id_number: sourceRow.spaceId,
+              name: sourceRow.name,
+              town: 'New New York'
+            })
+          }
+        })
+        expect(result).to.not.equal(null)
+      })
+
+      it('check empty inserts csv', () => {
+        checkFileLength(fifthSyncOutputDir, 'inserts', 'census.csv', 1)
+      })
+
+      it('check empty upserts csv', () => {
+        checkFileLength(fifthSyncOutputDir, 'upserts', 'census.csv', 1)
+      })
+
+      it('check empty deletes csv', () => {
+        checkFileLength(fifthSyncOutputDir, 'deletes', 'census.csv', 2)
+      })
+
+      it('check conflict csv has one row', () => {
+        checkFileLength(fifthSyncOutputDir, 'conflicts', 'census.csv', 3)
       })
     })
 
--- test/where-clause-tests.js
+++ test/where-clause-tests.js
@@ -10,43 +10,51 @@ describe('where clause generation', () => {
       label: 'equals string',
       where: { town: { equals: 'Springfield' } },
       expected: 'source.town = \'Springfield\'',
+      expectedInverted: 'not((source.town = \'Springfield\' and source.town is not null))',
       tableName: 'source'
     },
     {
       label: 'equals 10',
       where: { age: { equals: 10 } },
-      expected: 'age = 10'
+      expected: 'age = 10',
+      expectedInverted: 'not(age = 10)'
     },
     {
       label: 'equals true',
       where: { flowers: { equals: true } },
-      expected: 'flowers = true'
+      expected: 'flowers = true',
+      expectedInverted: 'not(flowers = true)'
     },
     {
       label: 'equals escaped string',
       where: { town: { equals: 'Spri\\ngfield' } },
       expected: 'source.town = E\'Spri\\\\ngfield\'',
+      expectedInverted: 'not((source.town = E\'Spri\\\\ngfield\' and source.town is not null))',
       tableName: 'source'
     },
     {
       label: 'equals O\'Reilly',
       where: { surname: { equals: 'O\'Reilly' } },
-      expected: 'surname = \'O\'\'Reilly\''
+      expected: 'surname = \'O\'\'Reilly\'',
+      expectedInverted: 'not((surname = \'O\'\'Reilly\' and surname is not null))'
     },
     {
       label: 'equals Smith or Jones',
       where: { surname: { equals: ['Smith', 'Jones'] } },
-      expected: '(surname = \'Smith\' OR surname = \'Jones\')'
+      expected: '(surname = \'Smith\' OR surname = \'Jones\')',
+      expectedInverted: 'not(((surname = \'Smith\' and surname is not null) OR (surname = \'Jones\' and surname is not null)))'
     },
     {
       label: 'greater than 5 and less than 10',
       where: { age: { moreThan: 5, lessThan: 10 } },
-      expected: 'age > 5 AND age < 10'
+      expected: 'age > 5 AND age < 10',
+      expectedInverted: 'not(age > 5 AND age < 10)'
     },
     {
       label: 'greater than or equal 0 and less than or equal 99',
       where: { age: { moreThanEquals: 0, lessThanEquals: 99 } },
-      expected: 'age >= 0 AND age <= 99'
+      expected: 'age >= 0 AND age <= 99',
+      expectedInverted: 'not(age >= 0 AND age <= 99)'
     },
     {
       label: 'equals Smith or Jones, greater than 5 and less than 10',
@@ -54,7 +62,8 @@ describe('where clause generation', () => {
         surname: { equals: ['Smith', 'Jones'] },
         age: { moreThan: 5, lessThan: 10 }
       },
-      expected: '(surname = \'Smith\' OR surname = \'Jones\') AND age > 5 AND age < 10'
+      expected: '(surname = \'Smith\' OR surname = \'Jones\') AND age > 5 AND age < 10',
+      expectedInverted: 'not(((surname = \'Smith\' and surname is not null) OR (surname = \'Jones\' and surname is not null)) AND age > 5 AND age < 10)'
     },
     {
       label: 'no where clause',
@@ -68,6 +77,15 @@ describe('where clause generation', () => {
       expect(where).to.eql(test.expected)
     })
   }
+
+  for (const test of tests) {
+    if (test.expected === '1=1') continue
+    it(`not(${test.label})`, () => {
+      const where = generateWhereClause.inverted(test.where, test.tableName)
+      expect(where).to.eql(test.expectedInverted)
+    })
+  }
+
 
   it('unknown comparator fails', () => {
     const f = () => generateWhereClause({ field: { cromulant: true } })
